b3e81fb0ee1dd3604389960ea365353c793328a7
ChanWhanPark <chananpark@github.com>
2021-03-06 12:52:56 +0900

    Add Naver Map

--- grad_project/app/src/main/java/com/chananpark/grad_project/MainActivity.java
+++ grad_project/app/src/main/java/com/chananpark/grad_project/MainActivity.java
@@ -1,14 +1,93 @@
 package com.chananpark.grad_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    // 네이버 지도 변수
+    private MapView mapView;
+    private static NaverMap naverMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 네이버 지도
+        mapView = (MapView)findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        // 배경 지도 선택
+        naverMap.setMapType(NaverMap.MapType.Navi);
+        // 건물 표시
+        naverMap.setLayerGroupEnabled(naverMap.LAYER_GROUP_BUILDING, true);
+        //위치 및 각도 조정
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(37.3392, 126.7335),   // 위치 지정
+                16,                                     // 줌 레벨
+                60,                                       // 기울임 각도
+                0                                     // 방향
+        );
+        naverMap.setCameraPosition(cameraPosition);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
